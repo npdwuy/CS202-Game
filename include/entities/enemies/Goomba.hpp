e2ebf992ec5c7c9c85d1044cf6e4bcd3bc2cf107
Nguyen Minh Khang <nmkhang_student@selab.hcmus.edu.vn>
2026-07-23 00:51:20 +0700

    feat(goomba): add initial Goomba class declaration

---
+++ include/entities/enemies/Goomba.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "entities/Enemy.hpp"
+
+class Goomba : public Enemy {
+public:
+    Goomba(sf::Vector2f position,
+           float speed,
+           float leftBoundary,
+           float rightBoundary);
+
+    ~Goomba() override = default;
+
+    void Update(sf::Time timePerFrame) override;
+    void Render(sf::RenderWindow& window) const override;
+
+    sf::FloatRect GetBounds() const override;
+
+    bool IsActive() const override;
+    void Deactivate() override;
+
+private:
+    sf::RectangleShape m_body;
+
+    float m_speed;
+    float m_leftBoundary;
+    float m_rightBoundary;
+
+    int m_direction;
+    bool m_active;
+};
