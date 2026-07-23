209667a02b7ba0e1fb8d0f82387d4335bc87ddd6
Nguyen Minh Khang <nmkhang_student@selab.hcmus.edu.vn>
2026-07-23 20:20:11 +0700

    feat(flying-enemy): add FlyingEnemy class declaration

---
+++ include/entities/enemies/FlyingEnemy.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "entities/Enemy.hpp"
+#include "entities/strategies/MovementStrategy.hpp"
+
+#include <memory>
+
+class FlyingEnemy : public Enemy {
+public:
+    FlyingEnemy(
+        sf::Vector2f position,
+        float speed,
+        std::unique_ptr<MovementStrategy> movementStrategy
+    );
+
+    ~FlyingEnemy() override = default;
+
+    void Update(sf::Time timePerFrame) override;
+    void Render(sf::RenderWindow& window) const override;
+
+    sf::FloatRect GetBounds() const override;
+
+    bool IsActive() const override;
+    void Deactivate() override;
+
+private:
+    sf::Texture m_texture;
+    sf::Sprite m_sprite;
+
+    std::unique_ptr<MovementStrategy> m_movementStrategy;
+
+    float m_speed;
+    float m_animationTime;
+
+    int m_currentFrame;
+    bool m_active;
+};
