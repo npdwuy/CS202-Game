c0af3d37fdd72acb487411fbd5eb5c43b756bfeb
Nguyen Minh Khang <nmkhang_student@selab.hcmus.edu.vn>
2026-08-03 01:08:40 +0700

    refactor(factory): delegate level enemy creation to EnemyFactory

--- src/factories/LevelObjectFactory.cpp
+++ src/factories/LevelObjectFactory.cpp
@@ -1,17 +1,14 @@
 #include "factories/LevelObjectFactory.hpp"
 
-#include "entities/enemies/BossEnemy.hpp"
-#include "entities/enemies/Goomba.hpp"
-#include "entities/enemies/Koopa.hpp"
+#include "factories/EnemyFactory.hpp"
+
 #include "entities/items/Coin.hpp"
 #include "entities/items/PowerUpPickup.hpp"
-#include "entities/strategies/PatrolStrategy.hpp"
 
 #if __has_include("entities/items/Mushroom.hpp")
 #include "entities/items/Mushroom.hpp"
 #endif
 
-#include <algorithm>
 #include <stdexcept>
 #include <string>
 
@@ -20,38 +17,14 @@ std::unique_ptr<Enemy> LevelObjectFactory::createEnemy(
     sf::Vector2f position,
     float tileSize,
     float levelWidth
-) const {
-    const float minimumX = std::max(0.f, position.x - tileSize * 2.f);
-    const float maximumX = std::min(
-        std::max(0.f, levelWidth - tileSize),
-        position.x + tileSize * 2.f
+) const
+{
+    return EnemyFactory::Create(
+        symbol,
+        position,
+        tileSize,
+        levelWidth
     );
-
-    switch (symbol) {
-        case 'G':
-            return std::make_unique<Goomba>(
-                position,
-                70.f,
-                std::make_unique<PatrolStrategy>(minimumX, maximumX)
-            );
-        case 'K':
-            return std::make_unique<Koopa>(
-                position,
-                55.f,
-                std::make_unique<PatrolStrategy>(minimumX, maximumX)
-            );
-        case 'B':
-            return std::make_unique<BossEnemy>(
-                sf::Vector2f(position.x, position.y - tileSize * 0.5f),
-                minimumX,
-                maximumX,
-                45.f
-            );
-        default:
-            throw std::invalid_argument(
-                "Unsupported enemy map symbol: " + std::string(1, symbol)
-            );
-    }
 }
 
 std::unique_ptr<Item> LevelObjectFactory::createItem(
@@ -82,8 +55,12 @@ std::unique_ptr<Item> LevelObjectFactory::createItem(
     }
 }
 
-bool LevelObjectFactory::isEnemySymbol(char symbol) {
-    return symbol == 'G' || symbol == 'K' || symbol == 'B';
+bool LevelObjectFactory::isEnemySymbol(char symbol)
+{
+    return symbol == 'G'
+        || symbol == 'K'
+        || symbol == 'E'
+        || symbol == 'B';
 }
 
 bool LevelObjectFactory::isItemSymbol(char symbol) {
