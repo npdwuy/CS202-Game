a44bfb53218d5ab61ca9f52de6b599d7769cf396
Rokieroo <vodat2553@gmail.com>
2026-08-13 00:20:09 +0700

    Change fireball cooldown to 1s and make item spawning smoother with ease-out

--- src/PlayState.cpp
+++ src/PlayState.cpp
@@ -319,7 +319,7 @@ void PlayState::Update(sf::Time timePerFrame) {
                 });
                 m_fireballs.push_back(std::move(fb));
                 m_player->triggerThrow();
-                m_fireballCooldown = 0.5f; // Cooldown 0.5s theo yêu cầu
+                m_fireballCooldown = 1.0f; // Cooldown 1s theo yêu cầu
             }
         }
     }
