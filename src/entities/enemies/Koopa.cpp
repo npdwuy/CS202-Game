1c2f9802f25b78ca791e49e5d2b45d908e49ee71
Nguyen Minh Khang <nmkhang_student@selab.hcmus.edu.vn>
2026-08-28 20:23:48 +0700

    feat(koopa): implement shell state transitions

--- src/entities/enemies/Koopa.cpp
+++ src/entities/enemies/Koopa.cpp
@@ -3,17 +3,23 @@
 
 #include <stdexcept>
 #include <utility>
+#include <cmath>
 
 Koopa::Koopa(
     sf::Vector2f position,
     float speed,
     std::unique_ptr<MovementStrategy> movementStrategy
 )
-    : m_movementStrategy(std::move(movementStrategy)),
-      m_speed(speed),
-      m_animationTime(0.f),
-      m_currentFrame(0),
-      m_active(true)
+: m_movementStrategy(std::move(movementStrategy)),
+  m_speed(speed),
+  m_animationTime(0.f),
+  m_currentFrame(0),
+  m_active(true),
+  m_state(State::Walking),
+  m_shellSpeed(450.f),
+  m_shellDirection(1),
+  m_flung(false),
+  m_velocity(0.f, 0.f)
 {
     if (!m_movementStrategy)
     {
@@ -38,12 +44,37 @@ void Koopa::Update(sf::Time timePerFrame)
         return;
     }
 
-    if (m_flung) {
-        m_velocity.y += 2000.f * timePerFrame.asSeconds();
-        m_sprite.move(m_velocity * timePerFrame.asSeconds());
-        if (m_sprite.getPosition().y > 2000.f) {
+    if (m_flung)
+    {
+        m_velocity.y +=
+            2000.f * timePerFrame.asSeconds();
+
+        m_sprite.move(
+            m_velocity * timePerFrame.asSeconds()
+        );
+
+        if (m_sprite.getPosition().y > 2000.f)
+        {
             m_active = false;
         }
+
+        return;
+    }
+
+    if (m_state == State::ShellIdle)
+    {
+        return;
+    }
+
+    if (m_state == State::ShellMoving)
+    {
+        const float distance =
+            m_shellSpeed
+            * static_cast<float>(m_shellDirection)
+            * timePerFrame.asSeconds();
+
+        m_sprite.move(distance, 0.f);
+
         return;
     }
 
@@ -54,7 +85,8 @@ void Koopa::Update(sf::Time timePerFrame)
     if (m_animationTime >= frameDuration)
     {
         m_animationTime = 0.f;
-        m_currentFrame = (m_currentFrame + 1) % 2;
+        m_currentFrame =
+            (m_currentFrame + 1) % 2;
 
         m_sprite.setTextureRect(
             sf::IntRect(
@@ -96,10 +128,65 @@ void Koopa::Deactivate()
     m_active = false;
 }
 
-void Koopa::Fling() {
+void Koopa::EnterShell()
+{
+    if (!m_active)
+    {
+        return;
+    }
+
+    m_state = State::ShellIdle;
+    m_animationTime = 0.f;
+    m_currentFrame = 0;
+}
+
+void Koopa::KickShell(int direction)
+{
+    if (!m_active)
+    {
+        return;
+    }
+
+    m_state = State::ShellMoving;
+
+    m_shellDirection =
+        direction >= 0 ? 1 : -1;
+}
+
+Koopa::State Koopa::GetState() const
+{
+    return m_state;
+}
+
+bool Koopa::IsWalking() const
+{
+    return m_state == State::Walking;
+}
+
+bool Koopa::IsShellIdle() const
+{
+    return m_state == State::ShellIdle;
+}
+
+bool Koopa::IsShellMoving() const
+{
+    return m_state == State::ShellMoving;
+}
+
+void Koopa::Fling()
+{
     m_flung = true;
-    m_velocity = {0.f, -500.f};
-    m_sprite.setScale(m_sprite.getScale().x, -m_sprite.getScale().y);
+    m_state = State::Walking;
+
+    m_velocity = {
+        0.f,
+        -500.f
+    };
+
+    m_sprite.setScale(
+        m_sprite.getScale().x,
+        -std::abs(m_sprite.getScale().y)
+    );
 }
 
 bool Koopa::IsFlung() const {
